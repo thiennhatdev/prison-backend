10846c59a45c7d735013003dae6c210f5e6ccfe7
user <user@usernoMacBook-Pro.local>
2026-07-04 15:17:52 +0700

    --no-edit

--- app/Exports/VisitationScheduleExport.php
+++ app/Exports/VisitationScheduleExport.php
@@ -5,6 +5,9 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use App\Models\VisitationSchedule;
+use App\Enums\PtEnum;
+use App\Enums\VisitGroupEnum;
+use App\Enums\ChildVisitGroupEnum;
 
 class VisitationScheduleExport implements FromCollection, WithHeadings
 {
@@ -17,22 +20,36 @@ class VisitationScheduleExport implements FromCollection, WithHeadings
     */
      public function collection()
     {
-        // return VisitationSchedule::query()
-        //     ->with(['customer'])
-        //     ->get()
-        //     ->map(function ($item, $index) {
         return $this->items->map(function ($item, $index) {
                 return [
                     'stt' => $index + 1,
-                    'customer' => $item->customer?->name,
-                    'prisoner_name' => $item->prisoner_name,
-                    'count' => $item->count,
                     'visitDate' =>\Carbon\Carbon::parse($item->visitDate)->format('d/m/Y'),
                     'visitTime' => \Carbon\Carbon::parse($item->visitTime)->format('H:i'),
                     'status' => [
                         'NOT_YET' => 'Sắp tới',
                         'DONE' => 'Đã thăm',
                     ][$item->status] ?? $item->status,
+                    'prisoner_name' => $item->prisoner_name,
+                    'prisoner_sex' => [
+                        'MALE' => 'Nam',
+                        'FEMALE' => 'Nữ',
+                    ][$item->prisoner_sex] ?? $item->prisoner_sex,
+                    'pt' => $item->pt?->label(),
+                    'customer' => $item->customer?->name,
+                    'username' => collect($item->relatives)
+                        ->pluck('username')
+                        ->implode(', '),
+                    'identification' => $item->identification,
+                    'count' => $item->count,
+                    'cccd' => collect($item->relatives)
+                        ->pluck('cccd')
+                        ->implode(', '),
+                    'phone' => collect($item->relatives)
+                        ->pluck('phone')
+                        ->implode(', '),
+                    'address' => collect($item->relatives)
+                        ->pluck('address')
+                        ->implode(', '),
                     ];
             });
     }
@@ -41,12 +58,20 @@ class VisitationScheduleExport implements FromCollection, WithHeadings
     {
         return [
             'STT',
-            'Thân nhân đăng ký',
-            'Phạm nhân',
-            'Số lượng',
             'Ngày thăm',
             'Giờ thăm',
             'Trạng thái',
+            'Phạm nhân',
+            'Giới tính',
+            'Phân trại',
+            'Thân nhân đăng ký',
+            'Người thân thích',
+            'Đại diện cơ quan, tổ chức, cá nhân khác',
+            'Số lượng',
+            'Số CCCD',
+            'Số điện thoại',
+            'Địa chỉ',
+            
         ];
     }
 }
